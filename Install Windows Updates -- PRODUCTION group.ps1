2bbffa5acf44409b20f86e1289e21fa35d096746
RonRunnerElowSum <97925118+RonRunnerElowSum@users.noreply.github.com>
2022-02-01 23:31:03 -0500

    Update Install Windows Updates -- PRODUCTION group.ps1

--- Install Windows Updates -- PRODUCTION group.ps1
+++ Install Windows Updates -- PRODUCTION group.ps1
@@ -213,6 +213,13 @@ function PunchIt () {
     $PatchGroupTest += $PatchGroupProductions
     
     $CurrentDate = Get-Date -Format d
+
+    if((Get-WmiObject -Class win32_systemenclosure -ComputerName $Env:ComputerName | Where-Object { $_.chassistypes -eq 9 -or $_.chassistypes -eq 10 -or $_.chassistypes -eq 14}) -and (Get-WmiObject -Class win32_battery -ComputerName $Env:ComputerName)){
+        $EndpointIsLaptop = $True
+    }
+    else{
+        $EndpointIsLaptop = $False
+    }
     
     if($PatchGroupProduction | Select-String $CurrentDate){
         Write-MSPLog -LogSource "MSP Patch Health" -LogType "Information" -LogMessage "Computer Name: $Env:COMPUTERNAME`r`nOS: $ClientOS"
@@ -224,7 +231,13 @@ function PunchIt () {
             InstallUpdatesWithForcedReboot
         }
         else{
-            InstallUpdatesWithNoReboot
+            if($EndpointIsLaptop){
+                InstallUpdatesWithNoReboot
+            }
+            else{
+                Write-MSPLog -LogSource "MSP Patch Health" -LogType "Information" -LogMessage "$Env:COMPUTERNAME is not a laptop and only patches between 12am and 5am...exiting..."
+                EXIT
+            }
         }
     }
     else{
